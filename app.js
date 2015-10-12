5dd51c03f8626aae4cad81796121aff666301b7e
kimberli <kimberlizhong@gmail.com>
2015-10-12 01:21:53 -0400

    add 404

--- app.js
+++ app.js
@@ -48,4 +48,27 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/freets', freets);
 
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// Development error handler, prints stacktraces
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('404', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// Production error handler, hides stacktraces
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500).end();
+});
+
 module.exports = app;
