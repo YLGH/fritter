95d054a47f7fa8ec0b61b556aa9b06d2f81266b3
kimberli <kimberlizhong@gmail.com>
2015-10-12 01:24:49 -0400

    fix error page

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('404', {
+    res.render('error', {
       message: err.message,
       error: err
     });
@@ -68,7 +68,8 @@ if (app.get('env') === 'development') {
 
 // Production error handler, hides stacktraces
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500).end();
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
